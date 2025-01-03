134dfc32adc1360a9860f8464e54a51def60cb2a
Piyush Gangwar <piyushgangwar7668@gmail.com>
2025-01-03 22:20:38 +0530

    GCD of two numbers

---
+++ Recursion/GCD.java
@@ -0,0 +1,35 @@
+class GCD{
+    // Method 1 
+    public static void gcd(int x, int y){
+        int nax=Math.min(x, y);
+        for(int i=nax;i>0;i--){
+            if(x%i==0 && y%i==0){
+                System.out.print(i+" ");
+                return;                
+            }
+        }
+    }
+    // method 2 
+    public static int gcd2(int x, int y){
+        while(x%y!=0){
+            int remainder=x%y;
+            x=y;
+            y=remainder;
+        }
+        return y;
+    }
+    // recursive
+    public static int gcd3(int x, int y){
+        if(y==0){
+            return x;
+        }
+        return gcd3(y,x%y);
+    }
+    public static void main(String[] args) {
+        gcd(14,16);
+        System.out.println(gcd2(14,16));
+        System.out.println(gcd3(14, 16));
+
+        
+    }
+}
