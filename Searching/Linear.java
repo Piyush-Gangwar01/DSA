321662db9aa67c9d2c714beda4feefdb72b76074
Piyush Gangwar <piyushgangwar7668@gmail.com>
2025-01-07 12:01:45 +0530

    Linear Search

---
+++ Searching/Linear.java
@@ -0,0 +1,13 @@
+public class Linear {
+    public static void main(String[] args) {
+        int arr[]={3,2,5,6,8,9};
+        int target=5;
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i]==target){
+                System.out.println(i);
+            }
+            
+        }
+        
+    }
+}
