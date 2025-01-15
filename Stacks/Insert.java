2c2b2612b06f4f7edd4794d7d39724fb0389858c
Piyush Gangwar <piyushgangwar7668@gmail.com>
2025-01-15 12:12:29 +0530

    Insert At Any Index

---
+++ Stacks/Insert.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Insert {
+    static Stack<Integer> add(Stack<Integer> st,int data,int idx){
+        Stack<Integer> temp=new Stack<>();
+        while(st.size()>idx){
+            temp.push((Integer) st.pop());
+        }
+        st.push(data);
+        while(temp.size()>0){
+            st.push(temp.pop());
+        }
+        return st;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of element:");
+        int n=sc.nextInt();
+        System.out.println("Enter element :");
+    
+        Stack<Integer> st=new Stack<>();
+        while (n>0) {
+            int input =sc.nextInt();
+            st.push(input);
+            n--;            
+        }
+        System.out.println("Enter you value u want to add ");
+        int data=sc.nextInt();
+        System.out.println("At what Index ");
+        int idx=sc.nextInt();
+
+        Stack ans=add(st, data, idx);
+        System.out.println(ans);
+
+        
+        
+    }
+}
